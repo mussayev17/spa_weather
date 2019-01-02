31419bd4b2b3d57a3471ad907c15f96ac5162aa1
mussayev14 <32241018+mussayev14@users.noreply.github.com>
2019-01-02 20:29:14 +0600

    Add files via upload

---
+++ utils/WeatherConditions.js
@@ -0,0 +1,47 @@
+export const weatherConditions = {
+  Rain: {
+    color: '#005BEA',
+    title: 'Дождливо',
+    subtitle: 'Захватите с собой зонт в дорогу)',
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    color: '#00d2ff',
+    title: 'Облачно',
+    subtitle: 'Холодно,но не в душе)',
+    icon: 'weather-cloudy'
+  },
+  Thunderstorm: {
+    color: '#616161',
+    title: 'Шторм',
+    icon: 'weather-lightning'
+  },
+  Clouds: {
+    color: '#1F1C2C',
+    title: 'Облачно',
+    subtitle: 'Хорошая погода ',
+    icon: 'weather-cloudy'
+  },
+
+  Snow: {
+    color: '#00d2ff',
+    title: 'Снег',
+    subtitle: 'Холодно',
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    color: '#076585',
+    title: 'Изморось',
+    icon: 'weather-hail'
+  },
+  Haze: {
+    color: '#66A6FF',
+    title: 'Мгла',
+    icon: 'weather-hail'
+  },
+  Mist: {
+    color: '#3CD3AD',
+    title: 'Туман',
+    icon: 'weather-fog'
+  }
+};
---
+++ utils/WreatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = '3352db8eb094658bf5704ba8c5f730';
