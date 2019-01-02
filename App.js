1eeffd5d189708984e1ed12c993729d31f085b6f
mussayev14 <32241018+mussayev14@users.noreply.github.com>
2019-01-02 20:29:52 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+const   API_KEY='3352db8eb094658bf5704ba8c5f73078';
+//import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Ошибка получения данных с сервера'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat, lon) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          isLoading: false
+        });
+      });
+  }
+
+  render() {
+    const { isLoading, weatherCondition, temperature } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}>Прогноз погоды</Text>
+          </View>
+        ) : (
+          <Weather weather={weatherCondition} temperature={temperature} />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFDE4'
+  },
+  loadingText: {
+    fontSize: 30
+  }
+});
